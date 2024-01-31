02c431a91c0953561ffcaf881864549c56de7267
HackDehZack <Zackseriousemail@gmail.com>
2024-01-30 20:58:01 -0500

    Update react-router-dom import in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LoginPage from './loginPage/LoginPage';
 import HeaderArea from './HeaderArea/HeaderArea';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
+      <Routes>
         <Route path='/LoginPage' exact={true} component={LoginPage} />
         <Route path='/HeaderArea' exact={true} component={HeaderArea} />
-      </Switch>
+      </Routes>
     </div>
   );
 }
